30f21366f45b10c444f3740fd1f6f8566d18e054
Colin Yetman <colinguitarist@gmail.com>
2025-02-25 15:54:20 -0330

    Jest Test for gameUtils.js - test for correct number of games in given genre - test for empty array of games when no games match genre - test case where search query is bigger then game data available - test that query does not edit original array data feedback

--- __tests__/utils/gameUtils.test.js
+++ __tests__/utils/gameUtils.test.js
@@ -1,11 +1,42 @@
 const { getGamesByGenre, getTopRatedGames, getGameDetailsById, selectRandomGameId, getHiddenGems } = require("../../utils/gameUtils");
+const { VideoGames } = require("../../utils/data");
+
 
 describe('Game Utility Functions', () => {
     describe('getGamesByGenre', () => {
-        // Test cases go here
+        test("returns the correct number of games for a given genre", () => {
+            const genre = "RPG";
+            const n = 3;
+            const result = getGamesByGenre(genre, n);
+
+            expect(result.length).toBeLessThanOrEqual(n);
+            expect(result.every(game => game.genre === genre)).toBe(true);
+        });
+        test("returns empty array if no games match genre", () => {
+            const genre = "Nonexistent Genre";
+            const result = getGamesByGenre(genre, 10);
+
+            expect(result.length).toBe(0);
+            expect(result).toStrictEqual([]);
+        });
+        test("checks case where query is larger then available data", () => {
+            const genre = "Action";
+            const n = 999999;
+            const result = getGamesByGenre(genre, n);
+
+            expect(result.length).toBeLessThanOrEqual(VideoGames.filter(game => game.genre === genre).length);
+            expect(result.every(game => game.genre === genre)).toBe(true);
+        });
+        test("checks that original array does not mutate", () => {
+            const genre = "Horror";
+            const OG = [...VideoGames];
+
+            getGamesByGenre(genre, 8);
+            expect(VideoGames).toStrictEqual(OG);
+        });
     });
 
-    describe('getTopRatedGames', () => {
+   /*  describe('getTopRatedGames', () => {
         // Test cases go here
     });
 
@@ -19,5 +50,5 @@ describe('Game Utility Functions', () => {
 
     describe('getHiddenGems', () => {
         // Test cases go here
-    });
+    }); */
 });
